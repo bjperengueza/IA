553e13d63860a39247a51472692a5e6d65174135
bjperengueza <brandon.jperen@gmail.com>
2022-06-10 05:09:15 -0500

    implementación del constructor

--- Proyecto/AlgoritmoBFS.py
+++ Proyecto/AlgoritmoBFS.py
@@ -8,4 +8,20 @@ class grafo:
     Clase que representa un objeto grafico segun los datos otorgados
 
     '''
+    # Constructor
+    def __init__(self, bordes, numero_nodos):
+        '''
+        Contructor que inicializa el problema de busqueda
 
+        @param bordes:  relaciona un nodo con su nodo vecino
+        @param n: numero de nodos del grafo.
+        
+        '''
+ 
+        # crea una lista de aristas para representar una lista de adyacencia
+        self.lista_adja = [[] for _ in range(numero_nodos)]
+ 
+        # agrega bordes al gráfico no dirigido
+        for (origen, destino) in bordes:
+            self.lista_adja[origen].append(destino)
+            self.lista_adja[destino].append(origen)
